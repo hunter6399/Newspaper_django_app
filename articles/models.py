047188b44a89d0af0f46b753c80dce638388e4b0
hunter6399 <man6399@gmail.com>
2020-07-17 21:07:13 +0530

    comments display +Deployment commit

--- articles/models.py
+++ articles/models.py
@@ -21,7 +21,11 @@ class Article(models.Model):
         return reverse('article_detail',args=[str(self.id)])
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article,on_delete=models.CASCADE)
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name= 'comments',
+    )
     comment=models.TextField(max_length=150)
     author=models.ForeignKey(
         get_user_model(),
